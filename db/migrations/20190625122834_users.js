29b641bebeb9fe04476eb7e281268303a31c26ce
MAllen07 <mrsmallen07@gmail.com>
2019-06-27 19:17:11 -0400

    added foreign keys to table

--- db/migrations/20190625122834_users.js
+++ db/migrations/20190625122834_users.js
@@ -1,41 +1,35 @@
-
 //TABLE
 
-exports.up = function (knex, Promise) {
-    return knex.schema.createTable("users", users =>{ //Table name 
-        users.increments();  //ID
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("users", users => {
+    //Table name
+    users.increments(); // primary
 
-        users
-            .string("username", 128)  //username
-            .notNullable()
-            .unique();
-        
-            
-        
+    users
+      .string("username", 128) //username
+      .notNullable()
+      .unique()
+      .references("id");
 
-        users
-            .string("password", 128) //password 
-            .notNullable()
-            
+    users
+      .string("password", 128) //password
+      .notNullable();
 
-        users
-            .string("firstName",) //full name 
-            .notNullable();
+    users
+      .string("firstName") //full name
+      .notNullable();
 
-        users
-            .string("lastName",) //last  name 
-            .notNullable();
+    users
+      .string("lastName") //last  name
+      .notNullable();
 
-        
-        users
-            .string("email") //email 
-            .notNullable()
-            .unique();
-
-
-    });
+    users
+      .string("email") //email
+      .notNullable()
+      .unique();
+  });
 };
 
-exports.down = function (knex, Promise) {
-    return knex.schema.dropTableIfExists("users");
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("users");
 };
