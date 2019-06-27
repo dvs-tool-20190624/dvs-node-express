ae2d35120203139d405a8f1f4bcf4541650eb4ce
MAllen07 <mrsmallen07@gmail.com>
2019-06-27 19:50:41 -0400

    renamed folders

--- Auth/restricted.js
+++ Auth/restricted.js
@@ -1,24 +1,22 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-const secrets = require('../config/secrets');
+const secrets = require("../config/secrets");
 
 module.exports = (req, res, next) => {
-    const token = req.headers.authorization;
+  const token = req.headers.authorization;
 
-    if (token) {
-        jwt.verify(token, secrets.jwtSecret, (err, decodeToken) => {
-            if (err) {
-                // invalid token
-                res.status(401).json({ message: 'Invalid Credentials' });
-            } else {
-                // valid token
-                req.user = { roles: decodeToken.roles, username: decodeToken.username };
-                next();
-            }
-
-            
-        });
-    } else {
-        res.status(400).json({ message: 'No token provided' });
-    }
+  if (token) {
+    jwt.verify(token, secrets.jwtSecret, (err, decodeToken) => {
+      if (err) {
+        // invalid token
+        res.status(401).json({ message: "Invalid Credentials" });
+      } else {
+        // valid token
+        req.user = { roles: decodeToken.roles, username: decodeToken.username };
+        next();
+      }
+    });
+  } else {
+    res.status(400).json({ message: "No token provided" });
+  }
 };
