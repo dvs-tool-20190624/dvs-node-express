e9d78e6eae5039ae1d228f7d62531126c99e8eba
MAllen07 <mrsmallen07@gmail.com>
2019-06-27 22:07:26 -0400

    adjusted table to make needed columns

--- db/migrations/20190627202112_financial.js
+++ db/migrations/20190627202112_financial.js
@@ -4,7 +4,8 @@ exports.up = function (knex, Promise) { // migrating files- creating table
     return knex.schema.createTable("financial", (table) => {
     
         table.increments(); // will set up id column - auto increments
-        table.string('item');
+        table.text('item').notNullable(); //must be added
+        table.text('content');
         table.string('amount');
         table.timestamps(true, true); // true for created at and true for updated 
      
